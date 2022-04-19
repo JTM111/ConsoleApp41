a8ad24df69e442dea02e4398211e28f6151e91aa
Jens Toft Madsen 1045 <JTM2842@ucl.dk>
2022-04-19 10:34:09 +0200

    -

--- JTM.cs
+++ JTM.cs
@@ -8,6 +8,11 @@ namespace ConsoleApp41a
     {
         public int Add(int a, int b)
         {
+            return a + b;
+        }
+
+        public int Sub(int a, int b)
+        {
             return a - b;
         }
     }
